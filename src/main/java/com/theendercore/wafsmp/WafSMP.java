35fe4f141bd874b4a92175fe0dd3634e7a35f48e
theendercore <theendercore@gmail.com>
2023-03-20 16:13:29 +0200

    fixed command bug

--- src/main/java/com/theendercore/wafsmp/WafSMP.java
+++ src/main/java/com/theendercore/wafsmp/WafSMP.java
@@ -3,7 +3,7 @@ package com.theendercore.wafsmp;
 import com.theendercore.wafsmp.commands.MapCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Objects;
+
 import java.util.logging.Logger;
 
 public final class WafSMP extends JavaPlugin {
@@ -12,8 +12,7 @@ public final class WafSMP extends JavaPlugin {
     public final Logger LOGGER = getLogger();
     @Override
     public void onEnable() {
-
-        Objects.requireNonNull(this.getServer().getPluginCommand("map")).setExecutor(new MapCommand());
+       this.getServer().getPluginCommand("map").setExecutor(new MapCommand());
         LOGGER.info("Plugin Loaded");
     }
 
