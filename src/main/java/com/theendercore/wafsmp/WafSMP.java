f63ce7bf3b5806cdd38ef2d9c08cbf88ede51cf3
theendercore <theendercore@gmail.com>
2023-08-08 14:52:53 +0300

    updated map command: - now is clickable - now can be configured - fancy colors

--- src/main/java/com/theendercore/wafsmp/WafSMP.java
+++ src/main/java/com/theendercore/wafsmp/WafSMP.java
@@ -1,17 +1,26 @@
 package com.theendercore.wafsmp;
 
 import com.theendercore.wafsmp.commands.MapCommand;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
-import java.util.logging.Logger;
 
 public final class WafSMP extends JavaPlugin {
 
 
-    public final Logger LOGGER = getLogger();
+    public final Logger LOGGER = LoggerFactory.getLogger(WafSMP.class.getSimpleName());
+    public static FileConfiguration config;
+
     @Override
     public void onEnable() {
+        config = getConfig();
+        config.addDefault("mapURL", "http://wafflessmp.apexmc.co:7652/");
+        config.options().copyDefaults(true);
+        saveConfig();
+
        this.getServer().getPluginCommand("map").setExecutor(new MapCommand());
         LOGGER.info("Plugin Loaded");
     }
--- src/main/java/com/theendercore/wafsmp/commands/MapCommand.java
+++ src/main/java/com/theendercore/wafsmp/commands/MapCommand.java
@@ -1,17 +1,25 @@
 package com.theendercore.wafsmp.commands;
 
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.format.Style;
+import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
+import static com.theendercore.wafsmp.WafSMP.config;
+
 public class MapCommand implements CommandExecutor {
-
-
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        sender.sendMessage( "You can see the map here:\nhttp://wafflessmp.apexmc.co:8123/");
+        String url = config.getString("mapURL");
+        if (url == null) return false;
+        sender.sendMessage(Component.text("You can see the map here:").style(Style.style().color(TextColor.color(16755200)).build()).appendNewline()
+                .append(Component.text(url).clickEvent(ClickEvent.clickEvent(ClickEvent.Action.OPEN_URL, url)))
+        );
         return true;
     }
 
